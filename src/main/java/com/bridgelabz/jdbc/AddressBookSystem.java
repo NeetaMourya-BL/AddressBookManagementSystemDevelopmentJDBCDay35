0eccbdd5d9c94a92c08153ac504709941bf4a98c
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-01-10 18:46:01 +0530

    welcome message in master branch

---
+++ src/main/java/com/bridgelabz/jdbc/AddressBookSystem.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.jdbc;
+
+/**
+ * Welcome to Address Book Program!
+ *
+ */
+public class AddressBookSystem 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Address Book Program" );
+    }
+}
