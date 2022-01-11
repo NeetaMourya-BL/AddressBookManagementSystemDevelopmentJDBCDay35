830fd3dfba619c4e48b2dd4fac5063544cfa8699
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-01-11 20:53:39 +0530

    [neeta mourya] added in createContactUC1 branch

--- src/main/java/com/bridgelabz/jdbc/AddressBookSystem.java
+++ src/main/java/com/bridgelabz/jdbc/AddressBookSystem.java
@@ -4,10 +4,8 @@ package com.bridgelabz.jdbc;
  * Welcome to Address Book Program!
  *
  */
-public class AddressBookSystem 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to Address Book Program" );
-    }
+public class AddressBookSystem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+	}
 }
